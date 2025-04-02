af9bba11131291daa2db7d427a09855b123b1f93
Raphaël Berly <raphaelberly@gmail.com>
2025-04-02 16:36:08 +0200

    Improve efficiency of chromium

--- main_search.py
+++ main_search.py
@@ -18,12 +18,15 @@ push = Push(**credentials['push'])
 
 options = webdriver.ChromeOptions()
 options.add_argument("--disable-extensions")
-options.add_argument("--no-sandbox")
+options.add_argument("--disable-plugins")
+options.add_argument("--disable-sync")
+options.add_argument("--disable-background-networking")
 options.add_argument("--disable-gpu")
-options.add_argument("--window-size=1000x800")
+options.add_argument("--no-sandbox")
 options.add_argument("--headless")
 
 try:
+    LOGGER.info('Starting Chrome driver')
     service = ChromeService(credentials.get('chromedriver') or ChromeDriverManager().install())
     driver = webdriver.Chrome(service=service, options=options)
 
@@ -35,7 +38,7 @@ try:
         time.sleep(2)
         for cookie_button in driver.find_elements(by='xpath', value='//*[text()="Continuer sans accepter"]'):
             cookie_button.click()
-        LOGGER.info(f'Refused cookies')
+        LOGGER.debug(f'Refused cookies')
         time.sleep(2)
 
         trip = driver.find_element(by='xpath', value=f'//*[@aria-labelledby="urn:trainline:flex:nonflexi"]/div[{train["target_result"]}]')
